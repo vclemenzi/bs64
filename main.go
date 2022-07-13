2115a26e549cae0741ce3d2cb6eeb2de04eaa1f9
Thebigbot <77632836+Thebigbot0000@users.noreply.github.com>
2022-07-13 16:48:01 +0200

    insert -e, -d options in the same switch of his long commands

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	switch os.Args[1] {
-	case "--encode":
+	case "--encode", "-e":
 		if len(os.Args) <= 3 {
 			fmt.Println("Usage: base64 --encode [-f|-s] <data>")
 			return;
@@ -26,41 +26,7 @@ func main() {
 		} else {
 			fmt.Println("Usage: base64 --encode [-f|-s] <data>")
 		}
-	case "--decode":
-		if len(os.Args) <= 3 {
-			fmt.Println("Usage: base64 --decode [-f|-s] <data>")
-			return;
-		}
-		if os.Args[2] == "-f" || os.Args[2] == "--file" {
-			content, err := ioutil.ReadFile(os.Args[3])
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			fmt.Println(base64.Decode(string(content)))
-		} else if os.Args[2] == "-s" || os.Args[2] == "--string" {
-			fmt.Println(base64.Decode(os.Args[3]))
-		} else {
-			fmt.Println("Usage: base64 --decode [-f|-s] <data>")
-		}
-	case "-e":
-		if len(os.Args) <= 3 {
-			fmt.Println("Usage: base64 --encode [-f|-s] <data>")
-			return;
-		}
-		if os.Args[2] == "-f" || os.Args[2] == "--file" {
-			content, err := ioutil.ReadFile(os.Args[3])
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			fmt.Println(base64.Encode(string(content)))
-		} else if os.Args[2] == "-s" || os.Args[2] == "--string" {
-			fmt.Println(base64.Encode(os.Args[3]))
-		} else {
-			fmt.Println("Usage: base64 --encode [-f|-s] <data>")
-		}
-	case "-d":
+	case "--decode", "-d":
 		if len(os.Args) <= 3 {
 			fmt.Println("Usage: base64 --decode [-f|-s] <data>")
 			return;
