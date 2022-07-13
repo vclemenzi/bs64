9937bf900a835ff4e57a24a3a882c4e82db78081
Thebigbot <77632836+aThebigbot@users.noreply.github.com>
2022-07-13 17:02:28 +0200

    Now you can set the output file

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"io/ioutil"
 	"github.com/aThebigbot/base64/base64"
+	"io/ioutil"
+	"os"
 )
 
 func main() {
@@ -12,34 +12,60 @@ func main() {
 	case "--encode", "-e":
 		if len(os.Args) <= 3 {
 			fmt.Println("Usage: base64 --encode [-f|-s] <data>")
-			return;
+			return
 		}
 		if os.Args[2] == "-f" || os.Args[2] == "--file" {
-			content, err := ioutil.ReadFile(os.Args[3])
-			if err != nil {
-				fmt.Println(err)
-				return
+			if len(os.Args) <= 4 {
+				content, err := ioutil.ReadFile(os.Args[3])
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				fmt.Println(base64.Encode(string(content)))
+			} else {
+				content, err := ioutil.ReadFile(os.Args[3])
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				ioutil.WriteFile(os.Args[4], []byte(base64.Encode(string(content))), 0644)
 			}
-			fmt.Println(base64.Encode(string(content)))
 		} else if os.Args[2] == "-s" || os.Args[2] == "--string" {
-			fmt.Println(base64.Encode(os.Args[3]))
+			if len(os.Args) <= 4 {
+				fmt.Println(base64.Encode(os.Args[3]))
+			} else {
+				ioutil.WriteFile(os.Args[4], []byte(base64.Encode(os.Args[3])), 0644)
+			}
 		} else {
 			fmt.Println("Usage: base64 --encode [-f|-s] <data>")
 		}
 	case "--decode", "-d":
 		if len(os.Args) <= 3 {
 			fmt.Println("Usage: base64 --decode [-f|-s] <data>")
-			return;
+			return
 		}
 		if os.Args[2] == "-f" || os.Args[2] == "--file" {
-			content, err := ioutil.ReadFile(os.Args[3])
-			if err != nil {
-				fmt.Println(err)
-				return
+			if len(os.Args) <= 4 {
+				content, err := ioutil.ReadFile(os.Args[3])
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				fmt.Println(base64.Decode(string(content)))
+			} else {
+				content, err := ioutil.ReadFile(os.Args[3])
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				ioutil.WriteFile(os.Args[4], []byte(base64.Decode(string(content))), 0644)
 			}
-			fmt.Println(base64.Decode(string(content)))
 		} else if os.Args[2] == "-s" || os.Args[2] == "--string" {
-			fmt.Println(base64.Decode(os.Args[3]))
+			if len(os.Args) <= 4 {
+				fmt.Println(base64.Decode(os.Args[3]))
+			} else {
+				ioutil.WriteFile(os.Args[4], []byte(base64.Decode(os.Args[3])), 0644)
+			}
 		} else {
 			fmt.Println("Usage: base64 --decode [-f|-s] <data>")
 		}
